88d0228ca2af507b36692ae77a791aae6f04e53d
spaghettiSyntax <spaghettiSyntax@gmail.com>
2021-01-11 09:30:42 -0800

    Number-Wizard

---
+++ Number-Wizard/IntroManager.cs
@@ -0,0 +1,149 @@
+﻿// (~˘▾˘)~ spaghettiSyntax
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour
+{
+    public static IntroManager instance;
+
+    [SerializeField] private Image sorcererDialogue = null;
+    [SerializeField] private TMP_Text sorcererText = null;
+    [SerializeField] private Image sorceressDialogue = null;
+    [SerializeField] private TMP_Text sorceressText = null;
+    [SerializeField] private Image warriorThoughtBubble = null;
+    [SerializeField] private TMP_Text warriorText = null;
+    [SerializeField] private TMP_Text startContinueButtonText = null;
+
+    [HideInInspector] public int introDialogueCounter;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        introDialogueCounter = -1;
+        CheckStartContinueButtonText();
+
+        sorcererDialogue.enabled = true;
+        sorcererText.enabled = true;
+        sorceressDialogue.enabled = false;
+        sorceressText.enabled = false;
+        warriorThoughtBubble.enabled = false;
+        warriorText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForUserInput();
+    }
+
+    private void CheckForUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            LoadNextDialogue();
+        }
+    }
+
+    public void StartContinueButton()
+    {
+        if (startContinueButtonText.text == "Continue")
+        {
+            LoadNextDialogue();
+        }
+        else
+        {
+            SceneLoader.instance.LoadNextScene();
+        }
+    }
+
+    public void LoadNextDialogue()
+    {
+        introDialogueCounter++;
+
+        CheckStartContinueButtonText();
+
+        switch (introDialogueCounter)
+        {
+            case 0:
+                sorcererDialogue.enabled = false;
+                sorcererText.enabled = false;
+
+                sorceressDialogue.enabled = true;
+                sorceressText.enabled = true;
+
+                sorceressText.text = "I am too...";
+                break;
+            case 1:
+                sorceressDialogue.enabled = false;
+                sorceressText.enabled = false;
+
+                warriorThoughtBubble.enabled = true;
+                warriorText.enabled = true;
+
+                warriorText.text = "Not going to tell them I could disarm it... could go for some food.";
+                warriorText.fontStyle = FontStyles.Italic;
+                break;
+            case 2:
+                warriorThoughtBubble.enabled = false;
+                warriorText.enabled = false;
+
+                sorcererDialogue.enabled = true;
+                sorcererText.enabled = true;
+
+                sorcererText.text = "How about a game?";
+                break;
+            case 3:
+                sorcererDialogue.enabled = false;
+                sorcererText.enabled = false;
+
+                sorceressDialogue.enabled = true;
+                sorceressText.enabled = true;
+
+                sorceressText.text = "I mean... sure?";
+                break;
+            case 4:
+                sorceressDialogue.enabled = false;
+                sorceressText.enabled = false;
+
+                warriorThoughtBubble.enabled = true;
+                warriorText.enabled = true;
+
+                warriorText.text = "I'd much rather eat...";
+                warriorText.fontStyle = FontStyles.Italic;
+                break;
+            case 5:
+                warriorThoughtBubble.enabled = false;
+                warriorText.enabled = false;
+
+                sorcererDialogue.enabled = true;
+                sorcererText.enabled = true;
+
+                sorcererText.text = "Think of a number between 1 and 1000...";
+                break;
+            case 6:
+                SceneLoader.instance.LoadNextScene();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void CheckStartContinueButtonText()
+    {
+        if (introDialogueCounter < 5)
+        {
+            startContinueButtonText.text = "Continue";
+        }
+        else
+        {
+            startContinueButtonText.text = "Start!";
+        }
+    }
+}
---
+++ Number-Wizard/NumberWizard.cs
@@ -0,0 +1,91 @@
+﻿// (~˘▾˘)~ spaghettiSyntax
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    public static NumberWizard instance;
+
+    [SerializeField] private Image sorcererDialogue = null;
+    [SerializeField] private TMP_Text sorcererText = null;
+    [SerializeField] private TMP_Text guessText = null;
+
+    [SerializeField] private Image warriorDialogue = null;
+    [SerializeField] private TMP_Text warriorText = null;
+
+    [SerializeField] private int min;
+    [SerializeField] private int max;
+    
+    private int guess;
+    public int guessCounter;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        guessCounter = 0;
+        sorcererDialogue.enabled = true;
+        warriorDialogue.enabled = false;
+        warriorText.enabled = false;
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void OnPressSuccess()
+    {
+        SceneLoader.instance.LoadNextScene();
+    }
+
+    void NextGuess()
+    {
+        guessCounter++;
+        int rand = Random.Range(1, 4);
+        guess = Random.Range(min, max + 1);
+        switch (rand)
+        {
+            case 1:
+                sorcererText.text = "It has to be";
+                guessText.text = guess.ToString() + "!";
+                break;
+            case 2:
+                sorcererText.text = "Is it";
+                guessText.text = guess.ToString() + "...";
+                break;
+            case 3:
+                sorcererText.text = "What about";
+                guessText.text = guess.ToString() + "?";
+                break;
+            default:
+                break;
+        }
+        
+        if (guessCounter == 5)
+        {
+            warriorDialogue.enabled = true;
+            warriorText.enabled = true;
+
+            warriorText.text = "Can we eat, yet?";
+        }
+        else
+        {
+            warriorDialogue.enabled = false;
+            warriorText.enabled = false;
+        }
+    }
+}
